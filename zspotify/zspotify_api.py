688489f086556f354589709ba8a0c74135cc79fa
shaunsingh <shaunsingh0207@gmail.com>
2023-05-07 15:46:35 -0400

    Update zspotify_api.py

--- zspotify/zspotify_api.py
+++ zspotify/zspotify_api.py
@@ -18,7 +18,7 @@ class ZSpotifyApi:
     def __init__(self,
                  sanitize=["\\", "/", ":", "*", "?", "'", "<", ">", '"'],
                  config_dir=Path.home() / ".zspotify",
-                 music_format="aiff",
+                 music_format="aac",
                  force_premium=False,
                  anti_ban_wait_time=5,
                  override_auto_wait=False,
@@ -235,9 +235,8 @@ class ZSpotifyApi:
         """Converts raw audio (ogg vorbis) to user specified format"""
         audio_segment = AudioSegment.from_file(audio_bytes)
 
-        # bitrate = "320k"
-        # audio_segment.export(output_path, format="aiff", bitrate=bitrate)
-        audio_segment.export(output_path, format="aiff")
+        bitrate = "320k"
+        audio_segment.export(output_path, format="aac", codec="aac_at", bitrate=bitrate)
 
     # INFO
     def get_audio_info(self, track_id, get_genres=False):
