e5a73a3793c3eecab2cab3d5eeea1f9bef4fffac
shaunsingh <shaunsingh0207@gmail.com>
2023-05-07 15:58:54 -0400

    Update zspotify_api.py

--- zspotify/zspotify_api.py
+++ zspotify/zspotify_api.py
@@ -18,7 +18,7 @@ class ZSpotifyApi:
     def __init__(self,
                  sanitize=["\\", "/", ":", "*", "?", "'", "<", ">", '"'],
                  config_dir=Path.home() / ".zspotify",
-                 music_format="m4a",
+                 music_format="wav",
                  force_premium=False,
                  anti_ban_wait_time=5,
                  override_auto_wait=False,
@@ -236,7 +236,7 @@ class ZSpotifyApi:
         audio_segment = AudioSegment.from_file(audio_bytes)
 
         # bitrate = "320k"
-        audio_segment.export(output_path, codec="alac_at") # format="aac", bitrate=bitrate)
+        audio_segment.export(output_path, format="wav")
 
     # INFO
     def get_audio_info(self, track_id, get_genres=False):
